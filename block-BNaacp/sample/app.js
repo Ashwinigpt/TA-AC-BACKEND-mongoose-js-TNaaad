1f5d665e0df3aa310146f01a7974890859f1a3af
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-04-23 14:05:55 +0530

    block-BNaacp

--- block-BNaacp/sample/app.js
+++ block-BNaacp/sample/app.js
@@ -33,8 +33,9 @@ app.post('/users', (req, res, next) => {
 
 // find
 
-app.get('/users', (req, res) => {
-    User.find({}, (err, user) => {
+app.get('/users/:id', (req, res) => {
+    var id = req.params.id;
+    User.find({_id:('62399a8e5eca681ea1a2c5b3')}, (err, user) => {
         console.log(err);
         res.json(user);
     });
@@ -42,8 +43,9 @@ app.get('/users', (req, res) => {
 
     // findOne
 
-app.get('/users', (req, res) => {
-    User.findOne({}, (err, user) => {
+app.get('/users/:id', (req, res) => {
+    User.findOne({_id:('62399a8e5eca681ea1a2c5b3')}, (err, user) => {
+        var id = req.params.id;
         console.log(err);
         res.json(user);
     });
@@ -51,8 +53,9 @@ app.get('/users', (req, res) => {
 
     // findById
 
-app.get('/users', (req, res) => {
-    User.findById({}, (err, user) => {
+app.get('/users/:id', (req, res) => {
+    var id = req.params.id;
+    User.findById({_id:('62399a8e5eca681ea1a2c5b3')}, (err, user) => {
         console.log(err);
         res.json(user);
     });
@@ -82,25 +85,30 @@ app.put('/users/:id', (req, res) => {
 
     // findByIdAndUpdate
 
-app.put('/users/:id', (req, res) => {
+app.put('/users/:id', (req, res, next) => {
     console.log(res.body);
     var id = req.params.id;
-    User.findByIdAndUpdate(id, req.body, {new: true}, (err, updatedProduct) => {
+    User.findByIdAndUpdate(id, req.body, {new: true}, (err, updatedUser) => {
         console.log(err);
-        res.json(updatedProduct);
+        res.json(updatedUser);
     });
 });   
 
 // delete route for deleting a user using id
 
-app.delete("/users/:id", (req, res) => {
-    var userId = "some id from database";
-    User.findByIdAndDelete(id, (err, user) => {
-      if (err) return next(err);
-      res.send("user deleted");
+app.delete("/users/:id", (req, res, next) => {
+    User.findByIdAndDelete(req.params.id, (err, deleteduser) => {
+      res.send(`${deleteduser.name} was deleted`);
     });
   });
 
+// error handlers
+
+app.use((req, res, next) => {
+    res.send("Page not found");
+});
+
+// server
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3k');
